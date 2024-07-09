962aef912e0f330ad56e4521494fd9154e334ff6
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-07-10 01:41:34 +0530

    config & DB models

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
+import connectCloudinary from './src/config/cloudinary.js';
 
 // Initialize the app
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
 
 // Apply middlewares
 app.use(express.json());
 app.use(cors());
+
+// initializing routes
+app.use("/api/song",songRouter)
 
 // Define a simple route
 app.get('/', (req, res) => {
