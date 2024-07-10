b851b97d2413346f01c1a9b2bc8fc44a814e2715
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-07-10 22:30:52 +0530

    Backend

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
+import albumRouter from './src/routes/albumRouter.js';
 
 // Initialize the app
 const app = express();
@@ -17,6 +18,8 @@ app.use(cors());
 
 // initializing routes
 app.use("/api/song",songRouter)
+app.use("/api/album",albumRouter)
+
 
 // Define a simple route
 app.get('/', (req, res) => {
